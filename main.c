30f2dd5fa18d6780d06ab638c65c27a588ac8b6b
meauxdal <68095521+meauxdal@users.noreply.github.com>
2026-05-26 15:29:47 -0400

    070

--- main.c
+++ main.c
@@ -492,9 +492,9 @@ static void report(uint8_t dmem_tvtype, int tv_type,
     if (is_ique) {
         printf("hardware  iQue Player\n");
     } else {
-        printf("hardware  N64  |  tvtype  0x%02X %-4s  |  reset  0x%02X %s\n",
-            (unsigned)dmem_tvtype, tv_type_str(tv_type),
-            (unsigned)dmem_resettype, reset_type_str(dmem_resettype));
+        printf("hardware: N64   |   reset: 0x%02X %s   |   tvtype: 0x%02X %-4s\n",
+            (unsigned)dmem_resettype, reset_type_str(dmem_resettype),
+            (unsigned)dmem_tvtype, tv_type_str(tv_type));            
     }
     printf("\n");
 
