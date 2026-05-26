cfb8c0979035f54e7919386f554ec5f55fcb6daa
meauxdal <68095521+meauxdal@users.noreply.github.com>
2026-05-26 15:49:50 -0400

    073

--- main.c
+++ main.c
@@ -492,7 +492,7 @@ static void report(uint8_t dmem_tvtype, int tv_type,
     if (is_ique) {
         printf("consoletype: iQue Player\n");
     } else {
-        printf("consoletype: Nintendo 64, resettype: 0x%02X %s, tvtype: 0x%02X %-4s\n",
+        printf("consoletype: N64, resettype: 0x%02X %s, tvtype: 0x%02X %-4s\n",
             (unsigned)dmem_resettype, reset_type_str(dmem_resettype),
             (unsigned)dmem_tvtype, tv_type_str(tv_type));            
     }
